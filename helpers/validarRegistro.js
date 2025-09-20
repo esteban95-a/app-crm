f3014f16fd3ac47e640b9ceb1fe50af1b1f5edb6
esteban95-a <dpachuri@cesde.net>
2025-09-20 14:49:40 -0500

    (develop) validacion de formulario con expresiones regulares

--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -1,2 +1,37 @@
+import { expresiones } from "./expresiones.js";
 let inputs = document.querySelectorAll("form div input")
-console.log(inputs);
+let arregloinputs = [...inputs];
+arregloinputs.map(input => { 
+    input.addEventListener("keyup", (e) => {
+        switch(e.target.id){
+            case "nombre":
+                if(expresiones.texto.test(e.target.value)){
+                    document.getElementById("nombre").style.borderColor = "green"
+                }
+                else{
+                    document.getElementById("nombre").style.borderColor = "red"
+                }
+                break;
+            case "apellido":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                }else{
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+             case "documento":
+                if(expresiones.numero.test(e.target.value)){
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                }else{
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+        }
+    })
+})
+
+
